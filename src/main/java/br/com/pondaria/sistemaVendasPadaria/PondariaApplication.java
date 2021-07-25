5fa44fd049ae9f541222f3c48194f97732bcfba0
Yuri Ressiguier <yuri_ressiguier@id.uff.br>
2021-07-25 16:19:52 -0300

    Base do projeto

---
+++ src/main/java/br/com/pondaria/sistemaVendasPadaria/PondariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.pondaria.sistemaVendasPadaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PondariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PondariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pondaria/sistemaVendasPadaria/PondariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pondaria.sistemaVendasPadaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PondariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
